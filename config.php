196433758dcab5033c971c2ee82fad1f8904b319
willwilson <w@willwilsondev.com>
2011-03-31 09:29:41 -0700

    moved config around

--- config.php
+++ config.php
@@ -3,7 +3,8 @@
 mysql_connect("localhost", "root", "");
 mysql_select_db("quixly") or error(mysql_error());
 
-define('local', true);
-
-$GLOBALS['date'] = gmdate('Y-m-d G:i:s');
+$GLOBALS['security'] = array(
+	'mcrypt' => array('cipher' => MCRYPT_RIJNDAEL_256, 'mode' => MCRYPT_MODE_ECB),
+	'key' => 'thorapi'
+);
 ?>
