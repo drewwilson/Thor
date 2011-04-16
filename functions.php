cbafd0902f6560a1ee24a4e30aa279ca875af835
willwilson <w@willwilsondev.com>
2011-04-16 12:53:51 -0700

    updated curl function

--- functions.php
+++ functions.php
@@ -36,15 +36,30 @@ function stripslashes_deep(&$value) {
 	}
 }
 
-function curl_request($type, $url, $query, $json=true){
+function curl_request($opts){
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
+	if ($opts['type'] == 'POST'){
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $opts['params']);
+	} else {
+		$opts['url'] .= '?';
+		foreach ($opts['params'] as $k => $v){
+			$url .= $k.'='.$v.'&';
+		}
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $opts['type']);
+	}
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+	curl_setopt($ch, CURLOPT_URL, $opts['url']);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	if (!empty($opts['headers'])){
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $opts['headers']);
+	}
 	$response = curl_exec($ch);
+	$info = curl_getinfo($ch);
 	curl_close($ch);
-	return ($json) ? json_decode($response) : $response;
+	return array('response' => $response, 'info' => $info);
 }
+
 
 ?>
